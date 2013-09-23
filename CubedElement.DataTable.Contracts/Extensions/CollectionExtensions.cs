3a0dcb38b8fa54c37410845eaba6131a33897ceb
kvernon <kvernon@cubedelement.com>
2013-09-22 21:55:33 -0500

    I hope I've added all the legal requirement needed. :/

--- CubedElement.DataTable.Contracts/Extensions/CollectionExtensions.cs
+++ CubedElement.DataTable.Contracts/Extensions/CollectionExtensions.cs
@@ -1,4 +1,25 @@
-﻿using System;
+﻿/*
+    JQuery DataTables Service Serializer Framework
+    Copyright (C) 2013 Kelly Vernon
+
+    http://www.cubedelement.com
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
@@ -10,18 +31,6 @@ namespace CubedElement.DataTable.Contracts.Extensions
     /// </summary>
     public static class CollectionExtensions
     {
-//        /// <summary>
-//        /// It simply provides a way to to know if the dictionary is null or empty; meaning it will return <c>true</c> if <c>null</c> or if the <see cref="IDictionary{TK, TV}.Count"/> is <c>0</c>.
-//        /// </summary>
-//        /// <typeparam name="TV"></typeparam>
-//        /// <typeparam name="TK"></typeparam>
-//        /// <param name="dictionary"></param>
-//        /// <returns></returns>
-//        public static bool IsNullOrEmpty<TK, TV>(this IDictionary<TK, TV> dictionary)
-//        {
-//            return ((ICollection<KeyValuePair<TK, TV>>)dictionary).IsNullOrEmpty();
-//        }
-
         /// <summary>
         /// It simply provides a way to to know if the list is null or empty; meaning it will return <c>true</c> if <c>null</c> or if the <see cref="IList{T}.Count"/> is <c>0</c>.
         /// </summary>
